b08cc3bcd3225b9635824ffdebbcdfdf9cc55855
Rich Perez <imperez5@gmail.com>
2023-08-16 21:05:41 -0400

    added play logic. started on play example.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
 import { create } from "./context";
 
-let context = create();
+const context = create();
+
+/** @type {AudioBuffer} buffer */
+let buffer;
+
 
 /**
  *
  * @param {String} file
  */
 const init = (file) => {
-    const buffer = new AudioBuffer();
+    window.fetch(file)
+        .then(response => response.arrayBuffer())
+        .then(arrayBuffer => context.decodeAudioData(arrayBuffer))
+        .then(audioBuffer => {
+            buffer = audioBuffer;
+        });
 }
 
 const play = () => {
-    context.play();
+    const source = context.createBufferSource();
+
+    source.buffer = audioBuffer;
+    source.connect(context.destination);
+    source.start();
 };
-const pause = () => {};
-const mute = () => {};
+const pause = () => { };
+const mute = () => { };
 
 export {
+    init,
     play,
     pause,
-
+    mute
 }
